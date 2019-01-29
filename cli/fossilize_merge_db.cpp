f91c8ee4492c9e859b73fb3f4fead4ec52fa0f04
Hans-Kristian Arntzen <post@arntzen-software.no>
2019-01-29 11:01:36 +0100

    Add a simple CLI tool which merges Fossilize DBs.

---
+++ cli/fossilize_merge_db.cpp
@@ -0,0 +1,51 @@
+/* Copyright (c) 2019 Hans-Kristian Arntzen
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include "fossilize_db.hpp"
+#include <memory>
+#include <vector>
+#include "layer/utils.hpp"
+
+using namespace Fossilize;
+
+static void print_help()
+{
+	LOGI("Usage: fossilize-merge-db append.foz [input1.foz] [input2.foz] ...\n");
+}
+
+int main(int argc, char **argv)
+{
+	std::vector<const char *> inputs;
+	if (argc < 3)
+	{
+		print_help();
+		return EXIT_FAILURE;
+	}
+
+	for (int i = 2; i < argc; i++)
+		inputs.push_back(argv[i]);
+
+	if (!merge_concurrent_databases(argv[1], inputs.data(), inputs.size()))
+		return EXIT_FAILURE;
+
+	return EXIT_SUCCESS;
+}
