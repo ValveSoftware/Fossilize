eb208b55c33c7d3bcb18f36e52d6ea5bbfe71179
Hans-Kristian Arntzen <post@arntzen-software.no>
2019-05-22 10:46:13 +0200

    Log progress every 0.1s rather than 0.5s.

--- cli/fossilize_replay.cpp
+++ cli/fossilize_replay.cpp
@@ -1472,7 +1472,7 @@ static int run_progress_process(const VulkanDevice::Options &,
 			}
 		}
 
-		std::this_thread::sleep_for(std::chrono::milliseconds(500));
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
 		ExternalReplayer::Progress progress = {};
 		auto result = replayer.poll_progress(progress);
 
