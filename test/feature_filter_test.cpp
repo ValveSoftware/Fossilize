ab7f03e826750f469a46c1ff52d0bc0874b65f5e
Hans-Kristian Arntzen <post@arntzen-software.no>
2023-09-18 18:23:39 +0200

    Add further tests for BuiltIn deprecated WG size.

--- test/feature_filter_test.cpp
+++ test/feature_filter_test.cpp
@@ -267,6 +267,209 @@ static const uint32_t spirv_blob[] =
 	0x000100fd, 0x00010038,
 };
 
+#if 0
+OpCapability Shader
+OpMemoryModel Logical GLSL450
+
+OpEntryPoint GLCompute %cs_local_1_1_1 "main_local_1_1_1"
+OpEntryPoint GLCompute %cs_local_id_1_1_1 "main_local_id_1_1_1"
+OpEntryPoint GLCompute %cs_spec_1_2_3 "main_spec_1_2_3"
+
+OpExecutionMode %cs_local_1_1_1 LocalSize 1 1 1
+OpExecutionModeId %cs_local_id_1_1_1 LocalSizeId %uint_1 %uint_1 %uint_1
+OpExecutionModeId %cs_spec_1_2_3 LocalSizeId %spec_uint_1 %spec_uint_2 %spec_uint_3
+
+OpDecorate %spec_uint_1 SpecId 0
+OpDecorate %spec_uint_2 SpecId 1
+OpDecorate %spec_uint_3 SpecId 2
+OpDecorate %wg BuiltIn WorkgroupSize
+
+%void = OpTypeVoid
+%3 = OpTypeFunction %void
+%uint = OpTypeInt 32 0
+%uvec3 = OpTypeVector %uint 3
+%global_spec_uint_1 = OpConstant %uint 1
+%global_spec_uint_2 = OpConstant %uint 2
+%global_spec_uint_3 = OpConstant %uint 3
+%spec_uint_1 = OpSpecConstant %uint 100
+%spec_uint_2 = OpSpecConstant %uint 100
+%spec_uint_3 = OpSpecConstant %uint 100
+%uint_1 = OpConstant %uint 1
+
+%wg = OpSpecConstantComposite %uvec3 %global_spec_uint_1 %global_spec_uint_2 %global_spec_uint_3
+
+%cs_local_1_1_1 = OpFunction %void None %3
+%cs_local_1_1_1_label = OpLabel
+OpReturn
+OpFunctionEnd
+
+%cs_local_id_1_1_1 = OpFunction %void None %3
+%cs_local_id_1_1_1_label = OpLabel
+OpReturn
+OpFunctionEnd
+
+%cs_spec_1_2_3 = OpFunction %void None %3
+%cs_spec_1_2_3_label = OpLabel
+OpReturn
+OpFunctionEnd
+#endif
+
+static const uint32_t spirv_blob_deprecated_wg_size[] =
+{
+	0x07230203, 0x00010600, 0x00070000, 0x00000013, 0x00000000, 0x00020011, 0x00000001, 0x0003000e,
+	0x00000000, 0x00000001, 0x0008000f, 0x00000005, 0x00000001, 0x6e69616d, 0x636f6c5f, 0x315f6c61,
+	0x315f315f, 0x00000000, 0x0008000f, 0x00000005, 0x00000002, 0x6e69616d, 0x636f6c5f, 0x695f6c61,
+	0x5f315f64, 0x00315f31, 0x0007000f, 0x00000005, 0x00000003, 0x6e69616d, 0x6570735f, 0x5f315f63,
+	0x00335f32, 0x00060010, 0x00000001, 0x00000011, 0x00000001, 0x00000001, 0x00000001, 0x0006014b,
+	0x00000002, 0x00000026, 0x00000004, 0x00000004, 0x00000004, 0x0006014b, 0x00000003, 0x00000026,
+	0x00000005, 0x00000006, 0x00000007, 0x00040047, 0x00000005, 0x00000001, 0x00000000, 0x00040047,
+	0x00000006, 0x00000001, 0x00000001, 0x00040047, 0x00000007, 0x00000001, 0x00000002, 0x00040047,
+	0x00000008, 0x0000000b, 0x00000019, 0x00020013, 0x00000009, 0x00030021, 0x0000000a, 0x00000009,
+	0x00040015, 0x0000000b, 0x00000020, 0x00000000, 0x00040017, 0x0000000c, 0x0000000b, 0x00000003,
+	0x0004002b, 0x0000000b, 0x0000000d, 0x00000001, 0x0004002b, 0x0000000b, 0x0000000e, 0x00000002,
+	0x0004002b, 0x0000000b, 0x0000000f, 0x00000003, 0x00040032, 0x0000000b, 0x00000005, 0x00000064,
+	0x00040032, 0x0000000b, 0x00000006, 0x00000064, 0x00040032, 0x0000000b, 0x00000007, 0x00000064,
+	0x0004002b, 0x0000000b, 0x00000004, 0x00000001, 0x00060033, 0x0000000c, 0x00000008, 0x0000000d,
+	0x0000000e, 0x0000000f, 0x00050036, 0x00000009, 0x00000001, 0x00000000, 0x0000000a, 0x000200f8,
+	0x00000010, 0x000100fd, 0x00010038, 0x00050036, 0x00000009, 0x00000002, 0x00000000, 0x0000000a,
+	0x000200f8, 0x00000011, 0x000100fd, 0x00010038, 0x00050036, 0x00000009, 0x00000003, 0x00000000,
+	0x0000000a, 0x000200f8, 0x00000012, 0x000100fd, 0x00010038,
+};
+
+#if 0
+OpCapability Shader
+OpMemoryModel Logical GLSL450
+
+OpEntryPoint GLCompute %cs_local_1_1_1 "main_local_1_1_1"
+OpEntryPoint GLCompute %cs_local_id_1_1_1 "main_local_id_1_1_1"
+OpEntryPoint GLCompute %cs_spec_1_2_3 "main_spec_1_2_3"
+
+OpExecutionMode %cs_local_1_1_1 LocalSize 1 1 1
+OpExecutionModeId %cs_local_id_1_1_1 LocalSizeId %uint_1 %uint_1 %uint_1
+OpExecutionModeId %cs_spec_1_2_3 LocalSizeId %spec_uint_1 %spec_uint_2 %spec_uint_3
+
+OpDecorate %global_spec_uint_1 SpecId 0
+OpDecorate %global_spec_uint_2 SpecId 1
+OpDecorate %global_spec_uint_3 SpecId 2
+OpDecorate %spec_uint_1 SpecId 3
+OpDecorate %spec_uint_2 SpecId 4
+OpDecorate %spec_uint_3 SpecId 5
+OpDecorate %wg BuiltIn WorkgroupSize
+
+%void = OpTypeVoid
+%3 = OpTypeFunction %void
+%uint = OpTypeInt 32 0
+%uvec3 = OpTypeVector %uint 3
+%global_spec_uint_1 = OpSpecConstant %uint 1
+%global_spec_uint_2 = OpSpecConstant %uint 2
+%global_spec_uint_3 = OpSpecConstant %uint 3
+%spec_uint_1 = OpSpecConstant %uint 100
+%spec_uint_2 = OpSpecConstant %uint 100
+%spec_uint_3 = OpSpecConstant %uint 100
+%uint_1 = OpConstant %uint 1
+
+%wg = OpSpecConstantComposite %uvec3 %global_spec_uint_1 %global_spec_uint_2 %global_spec_uint_3
+
+%cs_local_1_1_1 = OpFunction %void None %3
+%cs_local_1_1_1_label = OpLabel
+OpReturn
+OpFunctionEnd
+
+%cs_local_id_1_1_1 = OpFunction %void None %3
+%cs_local_id_1_1_1_label = OpLabel
+OpReturn
+OpFunctionEnd
+
+%cs_spec_1_2_3 = OpFunction %void None %3
+%cs_spec_1_2_3_label = OpLabel
+OpReturn
+OpFunctionEnd
+#endif
+
+static const uint32_t spirv_blob_deprecated_wg_size_spec[] =
+{
+	0x07230203, 0x00010600, 0x00070000, 0x00000013, 0x00000000, 0x00020011, 0x00000001, 0x0003000e,
+	0x00000000, 0x00000001, 0x0008000f, 0x00000005, 0x00000001, 0x6e69616d, 0x636f6c5f, 0x315f6c61,
+	0x315f315f, 0x00000000, 0x0008000f, 0x00000005, 0x00000002, 0x6e69616d, 0x636f6c5f, 0x695f6c61,
+	0x5f315f64, 0x00315f31, 0x0007000f, 0x00000005, 0x00000003, 0x6e69616d, 0x6570735f, 0x5f315f63,
+	0x00335f32, 0x00060010, 0x00000001, 0x00000011, 0x00000001, 0x00000001, 0x00000001, 0x0006014b,
+	0x00000002, 0x00000026, 0x00000004, 0x00000004, 0x00000004, 0x0006014b, 0x00000003, 0x00000026,
+	0x00000005, 0x00000006, 0x00000007, 0x00040047, 0x00000008, 0x00000001, 0x00000000, 0x00040047,
+	0x00000009, 0x00000001, 0x00000001, 0x00040047, 0x0000000a, 0x00000001, 0x00000002, 0x00040047,
+	0x00000005, 0x00000001, 0x00000003, 0x00040047, 0x00000006, 0x00000001, 0x00000004, 0x00040047,
+	0x00000007, 0x00000001, 0x00000005, 0x00040047, 0x0000000b, 0x0000000b, 0x00000019, 0x00020013,
+	0x0000000c, 0x00030021, 0x0000000d, 0x0000000c, 0x00040015, 0x0000000e, 0x00000020, 0x00000000,
+	0x00040017, 0x0000000f, 0x0000000e, 0x00000003, 0x00040032, 0x0000000e, 0x00000008, 0x00000001,
+	0x00040032, 0x0000000e, 0x00000009, 0x00000002, 0x00040032, 0x0000000e, 0x0000000a, 0x00000003,
+	0x00040032, 0x0000000e, 0x00000005, 0x00000064, 0x00040032, 0x0000000e, 0x00000006, 0x00000064,
+	0x00040032, 0x0000000e, 0x00000007, 0x00000064, 0x0004002b, 0x0000000e, 0x00000004, 0x00000001,
+	0x00060033, 0x0000000f, 0x0000000b, 0x00000008, 0x00000009, 0x0000000a, 0x00050036, 0x0000000c,
+	0x00000001, 0x00000000, 0x0000000d, 0x000200f8, 0x00000010, 0x000100fd, 0x00010038, 0x00050036,
+	0x0000000c, 0x00000002, 0x00000000, 0x0000000d, 0x000200f8, 0x00000011, 0x000100fd, 0x00010038,
+	0x00050036, 0x0000000c, 0x00000003, 0x00000000, 0x0000000d, 0x000200f8, 0x00000012, 0x000100fd,
+	0x00010038,
+};
+
+struct Test
+{
+	bool expected;
+	VkShaderStageFlagBits stage;
+	const char *entry;
+	uint32_t wg_size[3];
+	VkPipelineShaderStageCreateFlags flags;
+	uint32_t required_size;
+};
+
+static bool run_test(const FeatureFilter &filter, const Test &test, VkShaderModule module)
+{
+	VkPipelineShaderStageCreateInfo stage = { VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO };
+	VkPipelineShaderStageRequiredSubgroupSizeCreateInfo req =
+			{ VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_REQUIRED_SUBGROUP_SIZE_CREATE_INFO };
+
+	req.requiredSubgroupSize = test.required_size;
+
+	stage.module = module;
+	stage.stage = test.stage;
+	stage.pName = test.entry;
+	stage.flags = test.flags;
+	if (req.requiredSubgroupSize)
+		stage.pNext = &req;
+
+	static const VkSpecializationMapEntry entries[3] = {
+		{ 0, 0 * sizeof(uint32_t), sizeof(uint32_t) },
+		{ 1, 1 * sizeof(uint32_t), sizeof(uint32_t) },
+		{ 2, 2 * sizeof(uint32_t), sizeof(uint32_t) },
+	};
+
+	VkSpecializationInfo spec_info;
+	spec_info.pData = test.wg_size;
+	spec_info.dataSize = sizeof(test.wg_size);
+	spec_info.mapEntryCount = 3;
+	spec_info.pMapEntries = entries;
+
+	if (test.wg_size[0] || test.wg_size[1] || test.wg_size[2])
+		stage.pSpecializationInfo = &spec_info;
+
+	if (test.stage == VK_SHADER_STAGE_COMPUTE_BIT)
+	{
+		VkComputePipelineCreateInfo pipe = { VK_STRUCTURE_TYPE_COMPUTE_PIPELINE_CREATE_INFO };
+		pipe.stage = stage;
+		if (filter.compute_pipeline_is_supported(&pipe) != test.expected)
+			return false;
+	}
+	else
+	{
+		VkGraphicsPipelineCreateInfo pipe = { VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO };
+		pipe.stageCount = 1;
+		pipe.renderPass = (VkRenderPass)1;
+		pipe.pStages = &stage;
+		if (filter.graphics_pipeline_is_supported(&pipe) != test.expected)
+			return false;
+	}
+
+	return true;
+}
+
 int main()
 {
 	const char *ext = VK_EXT_MESH_SHADER_EXTENSION_NAME;
@@ -318,15 +521,7 @@ int main()
 	if (!filter.init(VK_API_VERSION_1_3, &ext, 1, &pdf2, &props2))
 		return EXIT_FAILURE;
 
-	static const struct test
-	{
-		bool expected;
-		VkShaderStageFlagBits stage;
-		const char *entry;
-		uint32_t wg_size[3];
-		VkPipelineShaderStageCreateFlags flags;
-		uint32_t required_size;
-	} tests[] = {
+	static const Test tests[] = {
 		// Sanity checks.
 		{ true, VK_SHADER_STAGE_COMPUTE_BIT, "main_local_1_1_1" },
 		{ false, VK_SHADER_STAGE_COMPUTE_BIT, "main_local_256_1_1" },
@@ -387,7 +582,30 @@ int main()
 		{ true, VK_SHADER_STAGE_TASK_BIT_EXT, "main_spec_1_2_3", { 8, 1, 1 }, VK_PIPELINE_SHADER_STAGE_CREATE_REQUIRE_FULL_SUBGROUPS_BIT, 8 },
 	};
 
+	// Test that BuiltIn always overrides.
+	static const Test deprecated_wg_tests[] =
+	{
+		{ true, VK_SHADER_STAGE_COMPUTE_BIT, "main_local_1_1_1" },
+		{ true, VK_SHADER_STAGE_COMPUTE_BIT, "main_local_id_1_1_1" },
+		{ true, VK_SHADER_STAGE_COMPUTE_BIT, "main_spec_1_1_1" },
+		{ true, VK_SHADER_STAGE_COMPUTE_BIT, "main_local_1_1_1", { 512, 1, 1 } },
+		{ true, VK_SHADER_STAGE_COMPUTE_BIT, "main_local_id_1_1_1", { 1, 512, 1 } },
+		{ true, VK_SHADER_STAGE_COMPUTE_BIT, "main_spec_1_2_3", { 1, 1, 512 } },
+	};
+
+	// Test that BuiltIn always overrides.
+	static const Test deprecated_wg_spec_tests[] =
+	{
+		{ true, VK_SHADER_STAGE_COMPUTE_BIT, "main_local_1_1_1" },
+		{ true, VK_SHADER_STAGE_COMPUTE_BIT, "main_local_id_1_1_1" },
+		{ true, VK_SHADER_STAGE_COMPUTE_BIT, "main_spec_1_1_1" },
+		{ false, VK_SHADER_STAGE_COMPUTE_BIT, "main_local_1_1_1", { 512, 1, 1 } },
+		{ false, VK_SHADER_STAGE_COMPUTE_BIT, "main_local_id_1_1_1", { 1, 512, 1 } },
+		{ false, VK_SHADER_STAGE_COMPUTE_BIT, "main_spec_1_2_3", { 1, 1, 512 } },
+	};
+
 	VkShaderModuleCreateInfo module_info = { VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO };
+
 	module_info.pCode = spirv_blob;
 	module_info.codeSize = sizeof(spirv_blob);
 	if (!filter.shader_module_is_supported(&module_info))
@@ -396,57 +614,25 @@ int main()
 	if (!filter.register_shader_module_info((VkShaderModule)1, &module_info))
 		return EXIT_FAILURE;
 
+	module_info.pCode = spirv_blob_deprecated_wg_size;
+	module_info.codeSize = sizeof(spirv_blob_deprecated_wg_size);
+	if (!filter.register_shader_module_info((VkShaderModule)2, &module_info))
+		return EXIT_FAILURE;
+
+	module_info.pCode = spirv_blob_deprecated_wg_size_spec;
+	module_info.codeSize = sizeof(spirv_blob_deprecated_wg_size_spec);
+	if (!filter.register_shader_module_info((VkShaderModule)3, &module_info))
+		return EXIT_FAILURE;
+
 	for (auto &test : tests)
-	{
-		VkPipelineShaderStageCreateInfo stage = { VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO };
-		VkPipelineShaderStageRequiredSubgroupSizeCreateInfo req =
-				{ VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_REQUIRED_SUBGROUP_SIZE_CREATE_INFO };
+		if (!run_test(filter, test, (VkShaderModule)1))
+			return EXIT_FAILURE;
 
-		req.requiredSubgroupSize = test.required_size;
+	for (auto &test : deprecated_wg_tests)
+		if (!run_test(filter, test, (VkShaderModule)2))
+			return EXIT_FAILURE;
 
-		stage.module = (VkShaderModule)1;
-		stage.stage = test.stage;
-		stage.pName = test.entry;
-		stage.flags = test.flags;
-		if (req.requiredSubgroupSize)
-			stage.pNext = &req;
-
-		static const VkSpecializationMapEntry entries[3] = {
-			{ 0, 0 * sizeof(uint32_t), sizeof(uint32_t) },
-			{ 1, 1 * sizeof(uint32_t), sizeof(uint32_t) },
-			{ 2, 2 * sizeof(uint32_t), sizeof(uint32_t) },
-		};
-
-		VkSpecializationInfo spec_info;
-		spec_info.pData = test.wg_size;
-		spec_info.dataSize = sizeof(test.wg_size);
-		spec_info.mapEntryCount = 3;
-		spec_info.pMapEntries = entries;
-
-		if (test.wg_size[0] || test.wg_size[1] || test.wg_size[2])
-			stage.pSpecializationInfo = &spec_info;
-
-		if (test.stage == VK_SHADER_STAGE_COMPUTE_BIT)
-		{
-			VkComputePipelineCreateInfo pipe = { VK_STRUCTURE_TYPE_COMPUTE_PIPELINE_CREATE_INFO };
-			pipe.stage = stage;
-			if (filter.compute_pipeline_is_supported(&pipe) != test.expected)
-			{
-				fprintf(stderr, "Failed test %d\n", int(&test - tests));
-				return EXIT_FAILURE;
-			}
-		}
-		else
-		{
-			VkGraphicsPipelineCreateInfo pipe = { VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO };
-			pipe.stageCount = 1;
-			pipe.renderPass = (VkRenderPass)1;
-			pipe.pStages = &stage;
-			if (filter.graphics_pipeline_is_supported(&pipe) != test.expected)
-			{
-				fprintf(stderr, "Failed test %d\n", int(&test - tests));
-				return EXIT_FAILURE;
-			}
-		}
-	}
+	for (auto &test : deprecated_wg_spec_tests)
+		if (!run_test(filter, test, (VkShaderModule)3))
+			return EXIT_FAILURE;
 }
