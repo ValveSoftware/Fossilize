7dc1a4a9e3070eaefa722c3aa937018a5ff4d605
Hans-Kristian Arntzen <post@arntzen-software.no>
2025-03-06 12:14:21 +0100

    Update the Android build script.

--- android_build.sh
+++ android_build.sh
@@ -9,42 +9,34 @@ njobs=`nproc`
 
 # Android
 if [ -z $ANDROID_SDK ]; then
-	ANDROID_SDK="$HOME/Android/Sdk"
+	ANDROID_SDK="$HOME/Android"
 fi
-
-# Android armeabi-v7a
-mkdir -p build-android-armeabi-v7a
-cd build-android-armeabi-v7a
-
-ANDROID_ABI=armeabi-v7a
-"$ANDROID_SDK"/cmake/*/bin/cmake \
-	.. \
-	-DANDROID_STL=c++_static \
-	-DANDROID_TOOLCHAIN=clang \
-	-DCMAKE_TOOLCHAIN_FILE="$ANDROID_SDK/ndk-bundle/build/cmake/android.toolchain.cmake" \
-	-DANDROID_ABI=$ANDROID_ABI \
-	-DANDROID_CPP_FEATURES=exceptions \
-	-DANDROID_ARM_MODE=arm \
-	-DCMAKE_BUILD_TYPE=$mode
-
-make -j$njobs
-cd ..
 
 # Android arm64-v8a
 mkdir -p build-android-arm64-v8a
 cd build-android-arm64-v8a
 
+if [ -z $ANDROID_NDK ]; then
+	echo "Must specify path to NDK in ANDROID_NDK".
+	exit 1
+fi
+
+unset ANDROID_HOME
+echo "Using ANDROID_SDK=$ANDROID_SDK"
+echo "Using ANDROID_NDK=$ANDROID_NDK"
+
 ANDROID_ABI=arm64-v8a
-"$ANDROID_SDK"/cmake/*/bin/cmake \
-	.. \
+cmake .. \
 	-DANDROID_STL=c++_static \
 	-DANDROID_TOOLCHAIN=clang \
-	-DCMAKE_TOOLCHAIN_FILE="$ANDROID_SDK/ndk-bundle/build/cmake/android.toolchain.cmake" \
+	-DCMAKE_TOOLCHAIN_FILE="$ANDROID_NDK/build/cmake/android.toolchain.cmake" \
 	-DANDROID_ABI=$ANDROID_ABI \
 	-DANDROID_CPP_FEATURES=exceptions \
 	-DANDROID_ARM_MODE=arm \
-	-DCMAKE_BUILD_TYPE=$mode
+	-DCMAKE_BUILD_TYPE=$mode \
+	-DANDROID_PLATFORM=30 \
+	-DFOSSILIZE_LAYER_APK=ON -G Ninja
 
-make -j$njobs
+ninja
 cd ..
 
